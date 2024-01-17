fc1c7130741a4a9dd7ee752ce7a471a41ff50dc7
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2024-01-17 11:24:55 +0100

    bundel 4: Delete Book.author

--- src/main/resources/dataForRealDB.sql
+++ src/main/resources/dataForRealDB.sql
@@ -1,22 +1,22 @@
-insert into book (title, author) values ('Oryx and Crake', ''); /*1*/
-insert into book (title, author) values ('The year of the flood', '');/*2*/
-insert into book (title, author) values ('MaddAddam', '');/*3*/
-insert into book (title, author) values ('1Q84', '');/*4*/
-insert into book (title, author) values ('De opwindvogelkronieken', '');/*5*/
-insert into book (title, author) values ('Design Patterns', '');/*6*/
-insert into book (title, author) values ('Anderland: De Stad Van Gouden Schaduw', ''); /*7*/
-insert into book (title, author) values ('Anderland:  Rivier van blauw vuur', ''); /*8*/
-insert into book (title, author) values ('Anderland:  Berg van zwart glas', ''); /*9*/
-insert into book (title, author)values ('Anderland:  Zee van zilveren licht', ''); /*10*/
-insert into book (title, author) values ('Neuromancer ', ''); /*11*/
-insert into book (title, author) values ('Snow crash', ''); /*12*/
-insert into book (title, author) values ('Rainbows’s end', ''); /*13*/
-insert into book (title, author) values ('Hard-boiled wonderland en het einde van de wereld', ''); /*14*/
-insert into book (title, author) values ('Ready Player one', ''); /*15*/
-insert into book (title, author) values ('Refactoring', ''); /*16*/
-insert into book (title, author) values ('Extreme Programming Explained', ''); /*17*/
-insert into book (title, author) values ('Implementation patterns', ''); /*18*/
-insert into book (title, author) values ('Clean code', ''); /*19*/
+insert into book (title) values ('Oryx and Crake'); /*1*/
+insert into book (title) values ('The year of the flood');/*2*/
+insert into book (title) values ('MaddAddam');/*3*/
+insert into book (title) values ('1Q84');/*4*/
+insert into book (title) values ('De opwindvogelkronieken');/*5*/
+insert into book (title) values ('Design Patterns');/*6*/
+insert into book (title) values ('Anderland: De Stad Van Gouden Schaduw'); /*7*/
+insert into book (title) values ('Anderland:  Rivier van blauw vuur'); /*8*/
+insert into book (title) values ('Anderland:  Berg van zwart glas'); /*9*/
+insert into book (title) values ('Anderland:  Zee van zilveren licht'); /*10*/
+insert into book (title) values ('Neuromancer '); /*11*/
+insert into book (title) values ('Snow crash'); /*12*/
+insert into book (title) values ('Rainbows’s end'); /*13*/
+insert into book (title) values ('Hard-boiled wonderland en het einde van de wereld'); /*14*/
+insert into book (title) values ('Ready Player one'); /*15*/
+insert into book (title) values ('Refactoring'); /*16*/
+insert into book (title) values ('Extreme Programming Explained'); /*17*/
+insert into book (title) values ('Implementation patterns'); /*18*/
+insert into book (title) values ('Clean code'); /*19*/
 
 insert into author (name) values ('Margaret Atwood'); /*1*/
 insert into author (name) values ('Haruki Murakami'); /*2*/
